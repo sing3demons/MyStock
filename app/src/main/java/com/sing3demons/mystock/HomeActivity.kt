09b9bafd944d4d67d8e98331f57980c513a0e610
sing3demons <sing3demons@live.com>
2020-10-21 19:34:05 +0700

    init project

--- app/src/main/java/com/sing3demons/mystock/HomeActivity.kt
+++ app/src/main/java/com/sing3demons/mystock/HomeActivity.kt
@@ -1,9 +1,11 @@
 package com.sing3demons.mystock
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayoutMediator
 import com.sing3demons.mystock.databinding.ActivityHomeBinding
 import com.sing3demons.mystock.ui.main.SectionsPagerAdapter
 
@@ -19,13 +21,22 @@ class HomeActivity : AppCompatActivity() {
         sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager, lifecycle)
 
         setupViewPage()
+        setupWidget()
+        setupTab()
 
+    }
 
-//        val tabs: TabLayout = findViewById(R.id.tabs)
-//        tabs.setupWithViewPager(binding.viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
+    private fun setupTab() {
+        TabLayoutMediator(binding.tabs, binding.viewPager, TabLayoutMediator.TabConfigurationStrategy{
+            tab, position ->
+        }).attach()
+//        binding.tabs.setupWithViewPager(binding.viewPager)
+    }
 
-        fab.setOnClickListener { view ->
+    private fun setupWidget() {
+//        val fab: FloatingActionButton = findViewById(R.id.fab)
+
+        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
@@ -34,6 +45,18 @@ class HomeActivity : AppCompatActivity() {
     private fun setupViewPage() {
         binding.viewPager.apply {
             adapter = sectionsPagerAdapter
+        }.also {
+            it.setPageTransformer(HorizontalFlipTransformation())
+            it.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    if (position == 0){
+                        binding.fab.visibility = View.INVISIBLE
+                    } else {
+                        binding.fab.visibility = View.INVISIBLE
+                    }
+                }
+            })
         }
         //val viewPager: ViewPager = findViewById(R.id.view_pager)
 //        viewPager.adapter = sectionsPagerAdapter
